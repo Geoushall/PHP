4b6719060a262ab1e7367b9535176d0d75585ab8
Geoushall <geoushall@outlook.com>
2017-03-21 19:46:08 +0800

    Update Disg_1.php

--- Disg_1.php
+++ Disg_1.php
@@ -2,12 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
+<title>Unkonwn</title>
 </head>
 
 <body>
 <?php
-//使用array_push()函数可向数组中添加元素，并循环输出添加后的数组
     
 	$a=array("red","green");
 	array_push($a,"blue","gray");
